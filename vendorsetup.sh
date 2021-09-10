82867470f132299f315ddc73d178efff63ab0f6f
AashiQ <77391250+aashiq-exe@users.noreply.github.com>
2021-09-10 18:05:24 +0530

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-### Using PE Telephony Repos ###
-rm -rf frameworks/opt/telephony/ && git clone https://github.com/PixelExperience/frameworks_opt_telephony.git frameworks/opt/telephony/
-rm -rf packages/services/Telephony/ && git clone https://github.com/PixelExperience/packages_services_Telephony.git packages/services/Telephony/
-rm -rf vendor/codeaurora/telephony/ && git clone https://github.com/PixelExperience/vendor_codeaurora_telephony.git vendor/codeaurora/telephony/
